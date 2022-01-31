ee49b0d7a450500a11857db618ee51a47cc6b857
jiwon79 <19-079@ksa.hs.kr>
2022-01-31 22:40:34 +0900

    feat: Add test link in landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -13,7 +14,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="#">0 Corporation</a>
+          <span>Welcome to </span>
+          <Link href="./listener-type">
+            <a>0 Corporation</a>
+          </Link>
         </h1>
 
         <p className={styles.description}>
