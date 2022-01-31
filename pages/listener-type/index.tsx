2af5777e4d30e033759416f4ea0a9b11312be73f
jiwon79 <19-079@ksa.hs.kr>
2022-01-31 23:01:27 +0900

    feat: Add test link in start page

--- pages/listener-type/index.tsx
+++ pages/listener-type/index.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link"
+
+
 export default function ListenerType() {
   return (
     <div>
       <h1>리스너 타입 테스트</h1>
+      <button>
+        <Link href={"/listener-type/test"}>
+          <a>테스트 하기</a>
+        </Link>
+      </button>
     </div>
   )
 }
