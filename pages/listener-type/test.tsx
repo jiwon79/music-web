f1b0b58bdffed204804edf29b0d8ee09f8cd1425
jiwon79 <19-079@ksa.hs.kr>
2022-02-01 17:13:32 +0900

    fix: fix error when current num over question list length

--- pages/listener-type/test.tsx
+++ pages/listener-type/test.tsx
@@ -1,20 +1,31 @@
-import { useState } from "react"
+import {useState} from "react"
+import {useRouter} from "next/router"
+
 import {questionList} from "../utils/constant";
 
 export default function TestPage() {
   const [currentNum, setCurrentNum] = useState(0);
+  const router = useRouter();
   const question = questionList[currentNum];
-  console.log(questionList[currentNum]);
+
+  const nextAction = () => {
+    if (currentNum === questionList.length-1) {
+      router.push('/listener-type/result/3').then(r => console.log(r));
+    } else {
+      setCurrentNum(currentNum+1);
+    }
+  }
 
   return (
     <div>
+      {currentNum}
       <div>
         {question.question}
       </div>
       <div>
         {question.answers}
       </div>
-      <button onClick={() => setCurrentNum(currentNum+1)}>
+      <button onClick={() => nextAction()}>
         다음 문제
       </button>
     </div>
