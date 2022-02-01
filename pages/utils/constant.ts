adf63b34dd132ae1c1ba0ca01842692d9baa1518
jiwon79 <19-079@ksa.hs.kr>
2022-02-01 16:37:10 +0900

    feat: Add question list data in constant

---
+++ pages/utils/constant.ts
@@ -0,0 +1,18 @@
+import {questionType} from "./types";
+
+export const questionList: Array<questionType> = [
+  {
+    question: 'question 1',
+    answers: [
+      'answer 1-1',
+      'answer 1-2'
+    ]
+  },
+  {
+    question: 'question 2',
+    answers: [
+      'answer 2-1',
+      'answer 2-2',
+    ]
+  }
+];
