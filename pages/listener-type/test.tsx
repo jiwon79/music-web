ec31ddf80064e179fbadde1fe200dc4095c157a6
jiwon79 <19-079@ksa.hs.kr>
2022-02-01 16:38:12 +0900

    feat: Add button that show next button

--- pages/listener-type/test.tsx
+++ pages/listener-type/test.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react"
+import {questionList} from "../utils/constant";
 
 export default function TestPage() {
-  const [questionNum, setQuestionNum] = useState(0);
-  console.log(questionNum);
+  const [currentNum, setCurrentNum] = useState(0);
+  const question = questionList[currentNum];
+  console.log(questionList[currentNum]);
 
   return (
     <div>
-      test page
+      <div>
+        {question.question}
+      </div>
+      <div>
+        {question.answers}
+      </div>
+      <button onClick={() => setCurrentNum(currentNum+1)}>
+        다음 문제
+      </button>
     </div>
   )
 }
