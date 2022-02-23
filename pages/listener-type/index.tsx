1f738b0e12b6b19e3e4ba43157ff22eb2f0c047e
jiwon79 <19-079@ksa.hs.kr>
2022-02-23 23:05:44 +0900

    feat: add title style

--- pages/listener-type/index.tsx
+++ pages/listener-type/index.tsx
@@ -1,14 +1,32 @@
 import Link from "next/link"
+import styles from "../../styles/listener-type/index.module.css"
+import Header from "./../../components/listener-type/Header"
 
 export default function ListenerType() {
   return (
     <div>
-      <h1>리스너 타입 테스트</h1>
-      <button>
-        <Link href={"/listener-type/test"}>
-          <a>테스트 하기</a>
-        </Link>
-      </button>
+      <div className={styles.container}>
+        <Header />
+        <div className={styles.title}>
+          L.istener<br/>
+          T.ype<br/>
+          T.est
+        </div>
+        <button>
+          <Link href={"/listener-type/test"}>
+            <a>테스트 하기</a>
+          </Link>
+        </button>
+      </div>
+      <table>
+        <tr>
+          <td>item1</td>
+          <td>item2</td>
+          <td>item3</td>
+          <td>item4</td>
+          <td>item5</td>
+        </tr>
+      </table>
     </div>
   )
 }
