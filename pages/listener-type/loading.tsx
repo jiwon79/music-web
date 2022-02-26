eaa507f3bd8246323a3e84a2e1b6315d823fad4a
jiwon79 <19-079@ksa.hs.kr>
2022-02-27 01:31:54 +0900

    feat: add loading page animation

---
+++ pages/listener-type/loading.tsx
@@ -0,0 +1,36 @@
+import Header from "../../components/listener-type/Header";
+import styles from "../../styles/listener-type/Loading.module.scss";
+import {useEffect, useRef, useState} from "react";
+
+export default function Loading() {
+  const loading = useRef<string>('결과 분석 중.');
+  const [loadingText, setLoadingText] = useState('결과 분석 중.');
+
+  useEffect(() => {
+    setInterval(function() {
+      if (loading.current === '결과 분석 중.') {
+        loading.current = '결과 분석 중..';
+        setLoadingText('결과 분석 중..');
+      } else if (loading.current === '결과 분석 중..') {
+        loading.current = '결과 분석 중...';
+        setLoadingText('결과 분석 중...');
+      } else {
+        loading.current = '결과 분석 중.';
+        setLoadingText('결과 분석 중.');
+      }
+      console.log(loading.current);
+      console.log(1);
+    }, 1000);
+  }, []);
+
+
+  return (
+    <div className={styles.container}>
+      <Header title={""} />
+      <div className={styles.content}>
+        <div>icon</div>
+        <p>{loadingText}</p>
+      </div>
+    </div>
+  )
+}
