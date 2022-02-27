16aeef757d2dc1587b016a6534dad1e253f27b22
jiwon79 <19-079@ksa.hs.kr>
2022-02-27 13:29:40 +0900

    feat: add type description in result page

---
+++ components/listener-type/Description/index.tsx
@@ -0,0 +1,28 @@
+import styles from "./style.module.scss";
+
+interface props {
+  title: string;
+  descList: Array<string>;
+}
+
+export default function Description({title, descList}: props) {
+  return (
+    <div>
+      <p className={styles.title}>{title}</p>
+      <div className={styles.detailList}>
+        <div className={styles.detail}>
+          <div className={styles.bullet}> </div>
+          <p>{descList[0]}</p>
+        </div>
+        <div className={styles.detail}>
+          <div className={styles.bullet}> </div>
+          <p>{descList[1]}</p>
+        </div>
+        <div className={styles.detail}>
+          <div className={styles.bullet}> </div>
+          <p>{descList[2]}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
--- pages/listener-type/result/[id].tsx
+++ pages/listener-type/result/[id].tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 import Header from "../../../components/listener-type/Header/";
 import MusicBar from "../../../components/listener-type/MusicBar/";
 import Menu from "../../../components/listener-type/Menu/";
+
 import styles from "../../../styles/listener-type/Result.module.scss"
 import {listenerTypeList} from "../../../utils/constant";
 import {listenerType} from "../../../utils/types";
+import Description from "../../../components/listener-type/Description";
 
 interface props {
   listenerType: listenerType
@@ -11,8 +15,8 @@ interface props {
 
 export default function ResultPage({ listenerType }: props) {
   return (
-    <div>
-      <div className={styles.container}>
+    <div className={styles.container}>
+      <div className={styles.content}>
         <Header title={"Hello, CLASSIC!"}/>
         <MusicBar />
         <div className={styles.music}>
@@ -23,6 +27,33 @@ export default function ResultPage({ listenerType }: props) {
         <div className={styles.listenerType}>
           <p className={styles.typeName}>{listenerType.name}</p>
           <p className={styles.typeDesc}>{listenerType.description}</p>
+        </div>
+
+        <Description
+          title={"나는 음악을 들을 때,"}
+          descList={listenerType.listenMusic}
+        />
+        <Description
+          title={"나는 이럴 때 음악을 찾는다!"}
+          descList={listenerType.findMusic}
+        />
+
+
+        <div className={styles.action}>
+          <Link href={"/listener-type/"}>
+            <a>
+              <div className={styles.button}>
+                <p>Retry?</p>
+              </div>
+            </a>
+          </Link>
+          <Link href={"/listener-type/"}>
+            <a>
+              <div className={styles.button}>
+                <p>Share!</p>
+              </div>
+            </a>
+          </Link>
         </div>
       </div>
       <Menu/>
