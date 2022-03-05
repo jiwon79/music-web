9ad41b82a830f8a156c8e6cb6bf3b348d5ff731c
jiwon79 <19-079@ksa.hs.kr>
2022-03-05 16:57:40 +0900

    feat: add music keywords with border components

--- pages/listener-type/result/[id].tsx
+++ pages/listener-type/result/[id].tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 import Header from "../../../components/listener-type/Header/";
 import MusicBar from "../../../components/listener-type/MusicBar/";
-import Menu from "../../../components/listener-type/Menu/";
+import Border from "../../../components/listener-type/Border/";
 
 import styles from "../../../styles/listener-type/Result.module.scss"
 import {listenerTypeList} from "../../../utils/constant";
@@ -16,50 +16,68 @@ interface props {
 export default function ResultPage({ listenerType }: props) {
   return (
     <div className={styles.container}>
-      <div className={styles.content}>
-        <Header title={"Hello, CLASSIC!"}/>
-        <MusicBar />
-        <div className={styles.music}>
-          <p className={styles.music__play}>00:03 / 2:35</p>
-          <p className={styles.music__title}>{listenerType.recommend[0].title}</p>
-          <p className={styles.music__artist}>{listenerType.recommend[0].artist}</p>
-        </div>
-        <div className={styles.listenerType}>
-          <p className={styles.typeName}>{listenerType.name}</p>
-          <p className={styles.typeDesc}>{listenerType.description}</p>
-        </div>
+      <Header title={"Hello, CLASSIC!"}/>
+      <MusicBar />
 
-        <Description
-          title={"나는 음악을 들을 때,"}
-          descList={listenerType.listenMusic}
-        />
-        <Description
-          title={"내가 음악을 찾을 때,"}
-          descList={listenerType.findMusic}
-        />
-        <Description
-          title={"좋아하는 음악을 즐길 때,"}
-          descList={listenerType.enjoyMusic}
-        />
+      <div className={styles.music}>
+        <p className={styles.music__play}>00:03 / 2:35</p>
+        <p className={styles.music__title}>{listenerType.recommend[0].title}</p>
+        <p className={styles.music__artist}>{listenerType.recommend[0].artist}</p>
+      </div>
 
-        <div className={styles.action}>
-          <Link href={"/listener-type/"}>
-            <a>
-              <div className={styles.button}>
-                <p>Retry?</p>
-              </div>
-            </a>
-          </Link>
-          <Link href={"/listener-type/"}>
-            <a>
-              <div className={styles.button}>
-                <p>Share!</p>
-              </div>
-            </a>
-          </Link>
+      <div className={styles.listenerType}>
+        <p className={styles.typeName}>{listenerType.name}</p>
+        <p className={styles.typeDesc}>{listenerType.description}</p>
+
+        <p className={styles.title}>음악과 함께할 때, KEYWORD!</p>
+        <div className={styles.keywords}>
+          <Border innerWidth={12} outerWidth={84}>
+            <p>{listenerType.keywords[0]}</p>
+          </Border>
+          <Border innerWidth={12} outerWidth={84}>
+            <p>{listenerType.keywords[1]}</p>
+          </Border>
+          <Border innerWidth={12} outerWidth={84}>
+            <p>{listenerType.keywords[2]}</p>
+          </Border>
         </div>
       </div>
-      <Menu/>
+
+
+      <Description
+        title={"나는 음악을 들을 때,"}
+        descList={listenerType.listenMusic}
+      />
+      <Description
+        title={"내가 음악을 찾을 때,"}
+        descList={listenerType.findMusic}
+      />
+      <Description
+        title={"좋아하는 음악을 즐길 때,"}
+        descList={listenerType.enjoyMusic}
+      />
+
+      <div className={styles.recommend}>
+        <p className={styles.title}>이런 음악과 잘 맞으실 거에요!</p>
+
+      </div>
+
+      <div className={styles.action}>
+        <Link href={"/listener-type/"}>
+          <a>
+            <div className={styles.button}>
+              <p>Retry?</p>
+            </div>
+          </a>
+        </Link>
+        <Link href={"/listener-type/"}>
+          <a>
+            <div className={styles.button}>
+              <p>Share!</p>
+            </div>
+          </a>
+        </Link>
+      </div>
     </div>
   )
 }
