57a123d828df42c15306047306187be1486a00a0
jiwon79 <19-079@ksa.hs.kr>
2022-03-06 01:22:48 +0900

    feat: add optional parameter on Border component

--- components/listener-type/Border/index.tsx
+++ components/listener-type/Border/index.tsx
@@ -3,13 +3,19 @@ import styles from "./style.module.scss";
 interface props {
   innerWidth: number;
   outerWidth: number;
-  children: JSX.Element
+  children: JSX.Element;
+  className?: string;
 }
 
-export default function Border({ innerWidth, outerWidth, children }: props) {
+export default function Border({
+    innerWidth = 0,
+    outerWidth = 0,
+    children = null,
+    className = ''
+  }: props) {
   return (
     <div
-      className={styles.container}
+      className={`${styles.container} ${className}`}
       style={{
         width: outerWidth,
         height: outerWidth,
