9cd5ca70d1c5f4517bbb4f79dfd2e8cea5972f88
jiwon79 <19-079@ksa.hs.kr>
2022-03-25 21:54:10 +0900

    feat: add nested menu

--- components/common/Layout/MainLayout/sidebar.tsx
+++ components/common/Layout/MainLayout/sidebar.tsx
@@ -1,7 +1,17 @@
 import Link from "next/link";
+import {useState} from "react";
 import styles from "./sidebar.module.scss"
 
 export default function SideBar({isOpen, handleSideOption}) {
+  const [pastMenuIsOpen, setPastMenuIsOpen] = useState<boolean>(false);
+  const [currentMenuIsOpen, setCurrentMenuIsOpen] = useState<boolean>(false);
+  const handlePastMenuIsOpen = () => {
+    setPastMenuIsOpen(!pastMenuIsOpen);
+  }
+  const handleCurrentMenuIsOpen = () => {
+    setCurrentMenuIsOpen(!currentMenuIsOpen);
+  }
+
   return (
     <div className={`${isOpen ? styles.open : styles.close} ${styles.menu}`}>
       <button onClick={() => handleSideOption('NULL')}>
@@ -14,14 +24,38 @@ export default function SideBar({isOpen, handleSideOption}) {
           </Link>
         </li>
         <li>
+          <button onClick={() => handlePastMenuIsOpen()}>
+            +
+          </button>
           <Link href={"/past"}>
             <a>과거</a>
           </Link>
+          {pastMenuIsOpen ?
+            <ul>
+              <li>
+                aa
+              </li>
+              <li>
+                bb
+              </li>
+            </ul>
+          : ''
+          }
         </li>
         <li>
+          <button onClick={() => handleCurrentMenuIsOpen()}>
+            +
+          </button>
           <Link href={"/current"}>
             <a>현재</a>
           </Link>
+          {currentMenuIsOpen ?
+            <ul>
+              <li>aa</li>
+              <li>bb</li>
+            </ul>
+          : ''
+          }
         </li>
         <li>
           <Link href={"/game"}>
