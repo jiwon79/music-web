858da3ebe91106816332ea76004291b3e258dd28
jiwon79 <19-079@ksa.hs.kr>
2022-03-26 15:51:45 +0900

    feat: add listener type enums

---
+++ utils/enum.ts
@@ -0,0 +1,10 @@
+export enum ListenerTypeName {
+  CHERISH = 'cherish',
+  INNOCENT = 'innocent',
+  ANTIQUE = 'antique',
+  BIZARRE = 'bizarre',
+  TENDER = 'tender',
+  SPONTANEOUS = 'spontaneous',
+  PUZZLING = 'puzzling',
+  CANDID = 'candid',
+}
