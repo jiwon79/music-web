258f95b1de808dd5c4a6e90fd6b5f919b5d802b1
jiwon79 <19-079@ksa.hs.kr>
2022-06-06 22:00:31 +0900

    fix: fix react quill editor

--- components/common/Editor/index.tsx
+++ components/common/Editor/index.tsx
@@ -1,61 +1,68 @@
-import dynamic from 'next/dynamic'
-import {useState} from "react";
+import {useMemo, useRef} from "react";
+import 'react-quill/dist/quill.snow.css';
+import ReactQuill from "react-quill";
 
-const QuillNoSSRWrapper = dynamic(import('react-quill'), {
-  ssr: false,
-  loading: () => <p>Loading ...</p>,
-})
+export default function Editor({setHtmlStr}) {
+  const QuillNoSSRWrapper = typeof window === 'object' ? require('react-quill') : () => false
+  const editorRef = useRef<ReactQuill>(null)
+  console.log(editorRef.current ? editorRef.current.getEditor().getContents() : 'null')
 
-const modules = {
-  toolbar: [
-    [{ 'header': ['1', '2', '3', '4', '5', '6', false]}],
-    [{ header: '1' }, { header: '2' }, {header: '3'}, { font: [] }],
-    [{ align: []}],
-    [{ size: [] }],
-    [{ color: ['#000', '#fff', '#c8c8c8']}],
-    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-    [
-      { list: 'ordered' },
-      { list: 'bullet' },
-      { indent: '-1' },
-      { indent: '+1' },
-    ],
-    ['link', 'image', 'video'],
-    ['clean'],
-  ],
-  clipboard: {
-    // toggle to add extra line breaks when pasting HTML:
-    matchVisual: false,
-  },
-}
+  const insert = () => {
+    const edi = editorRef.current.getEditor();
+    const range = edi.getSelection();
+    const position = range ? range.index : 0;
+    edi.insertText(position, 'Hello, World! ')
+  }
 
-const formats = [
-  'header',
-  'font',
-  'size',
-  'bold',
-  'italic',
-  'underline',
-  'strike',
-  'blockquote',
-  'list',
-  'bullet',
-  'indent',
-  'link',
-  'image',
-  'video',
-]
+  const modules = useMemo(() => ({
+    toolbar: {
+      container: [
+        [{header: ['1', '2', '3', '4', '5', '6', false]}],
+        [{size: []}],
+        [{align: []}],
+        [
+          {color: ['#000', '#fff', '#c8c8c8']},
+          {background: ['#00ff00']}
+        ],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [
+          {list: 'ordered'},
+          {list: 'bullet'},
+          {indent: '-1'},
+          {indent: '+1'},
+        ],
+        ['link', 'image', 'video'],
+        ['clean'],
+        ['insert']
+      ],
+      handlers: {
+        insert: insert
+      },
+    },
+    clipboard: {
+      matchVisual: false
+    },
+  }), []);
 
-export default function Editor() {
-  const [text, setText] = useState('');
-  console.log(text);
+  const format = [
+    "header", "font", "size",
+    "bold", "italic", "underline", "strike", "blockquote",
+    "align",
+    "list", "bullet",
+    "indent",
+    "background", "color",
+    "link", "image", "video",
+    "insert"
+  ]
 
-  return <QuillNoSSRWrapper
-    theme={'snow'}
-    id={'description'}
-    placeholder={'설명을 입력해주세요'}
-    onChange={(value) => setText(value)}
-    modules={modules}
-    formats={formats}
-  />
+  return (
+    <QuillNoSSRWrapper
+      theme={'snow'}
+      ref={editorRef}
+      placeholder={'설명을 입력해주세요'}
+      onChange={(value) => setHtmlStr(value)}
+      modules={modules}
+      format={format}
+    />
+  )
 }
--- pages/write/index.tsx
+++ pages/write/index.tsx
@@ -1,10 +1,14 @@
 import Editor from "../../components/common/Editor";
+import {useState} from "react";
 
 export default function WritePage() {
+  const [htmlStr, setHtmlStr] = useState<string>('');
+
   return (
     <div>
       글쓰기 페이지
-      <Editor/>
+      <Editor setHtmlStr={setHtmlStr}/>
+      <div dangerouslySetInnerHTML={{__html: htmlStr}}/>
     </div>
   )
 }
