8815ce4392b136d1c83a93e75954142f09fb45bb
jiwon79 <19-079@ksa.hs.kr>
2022-06-20 13:40:31 +0900

    feat: add labeling game result dict type

--- utils/game/festival/type.ts
+++
@@ -1,7 +0,0 @@
-import {ResultStandard} from "./enums";
-
-export interface questionType {
-  question: string,
-  answers: Array<string>,
-  result: Array<Array<ResultStandard>>
-}
---
+++ utils/game/festival/types.ts
@@ -0,0 +1,25 @@
+import {ResultStandard} from "./enums";
+
+export interface questionType {
+  question: string,
+  answers: Array<string>,
+  result: Array<Array<ResultStandard>>
+}
+
+export interface resultDictType {
+  [ResultStandard.LIVELY]: number,
+  [ResultStandard.PLAN]: number,
+  [ResultStandard.CALM]: number,
+  [ResultStandard.FREE]: number,
+  [ResultStandard.MOOD]: number,
+  [ResultStandard.OTAKU]: number,
+}
+
+export const defaultResultDict = {
+  [ResultStandard.LIVELY]: 0,
+  [ResultStandard.PLAN]: 0,
+  [ResultStandard.CALM]: 0,
+  [ResultStandard.FREE]: 0,
+  [ResultStandard.MOOD]: 0,
+  [ResultStandard.OTAKU]: 0,
+}
