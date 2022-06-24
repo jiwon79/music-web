f3332da917a8a279857f3d6cefab6b92d9082771
jiwon79 <19-079@ksa.hs.kr>
2022-06-24 19:07:49 +0900

    feat: add useLoad custom hook

--- pages/game/festival/loading.tsx
+++ pages/game/festival/loading.tsx
@@ -1,7 +1,16 @@
+import useLoad from "../../../utils/hooks/useLoad";
+import {useEffect} from "react";
+
 export default function LoadingPage() {
+  const {loadText, loadAction} = useLoad(4);
+  useEffect(() => {
+    loadAction();
+  }, []);
+  
   return (
     <div>
-      loading page
+      <p>loading page</p>
+      <p>{loadText}</p>
     </div>
   )
 }
