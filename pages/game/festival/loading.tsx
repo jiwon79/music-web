0f22e85f0b3cb6eeb29c4eaea363732da7cd87a5
jiwon79 <19-079@ksa.hs.kr>
2022-06-27 20:34:20 +0900

    feat: Add get result dict and calc type logic on loading page

--- pages/game/festival/loading.tsx
+++ pages/game/festival/loading.tsx
@@ -1,10 +1,33 @@
 import useLoad from "../../../utils/hooks/useLoad";
 import {useEffect} from "react";
+import {useRouter} from "next/router";
+import {ResultStandard} from "../../../utils/game/festival/enums";
 
 export default function LoadingPage() {
+  const router = useRouter();
   const {loadText, loadAction} = useLoad(4);
+
   useEffect(() => {
-    loadAction();
+    if (!router.isReady)  return null;
+    const resultType = getFestivalType(JSON.parse(router.query.resultDict as string))
+    console.log(resultType)
+  }, [router.isReady]);
+
+  const getFestivalType = (resultDict: Record<string, number>) => {
+    var typeNum = 0;
+    if (resultDict[ResultStandard.LIVELY] > resultDict[ResultStandard.CALM])  typeNum += 4;
+    if (resultDict[ResultStandard.PLAN] > resultDict[ResultStandard.FREE])  typeNum += 2;
+    if (resultDict[ResultStandard.OTAKU] > resultDict[ResultStandard.MOOD]) typeNum += 1;
+    return typeNum;
+  }
+
+  useEffect(() => {
+    const load = async () => {
+      await loadAction();
+      // const festivalType = getFestivalType(resultDict);
+      // console.log(festivalType);
+    }
+    load();
   }, []);
   
   return (
