48004a7fea72c9cdf1961bba7dab5957929f80b3
jiwon79 <19-079@ksa.hs.kr>
2022-06-28 14:50:02 +0900

    feat: Add festival type result page

--- pages/game/festival/result/[id].tsx
+++ pages/game/festival/result/[id].tsx
@@ -1,7 +1,42 @@
-export default function ResultPage() {
+import {festivalTypeMap} from "../../../../utils/game/festival/constant";
+import {FestivalType} from "../../../../utils/game/festival/type";
+
+interface ResultPageProps {
+  festivalType: FestivalType
+}
+
+export default function ResultPage({ festivalType }: ResultPageProps) {
   return (
     <div>
-      result page
+      <p>{festivalType.name}</p>
+      <ul>
+        {festivalType.descriptions.map((desc, idx) =>
+          <li key={idx}>{desc}</li>
+        )}
+      </ul>
     </div>
   )
+}
+
+export function getStaticProps({ params }) {
+  return {
+    props: {
+      festivalType: festivalTypeMap[params.id]
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  return  {
+    paths: [
+      { params: { id: '1' }},
+      { params: { id: '2' }},
+      { params: { id: '3' }},
+      { params: { id: '4' }},
+      { params: { id: '5' }},
+      { params: { id: '6' }},
+      { params: { id: '7' }},
+    ],
+    fallback: false
+  }
 }
