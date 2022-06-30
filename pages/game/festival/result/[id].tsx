5d0a907ba2eebde91a1528acf78a6d3868ea9dd0
jiwon79 <19-079@ksa.hs.kr>
2022-06-30 12:32:54 +0900

    feat: Add ShareButton on festival result page

--- pages/game/festival/result/[id].tsx
+++ pages/game/festival/result/[id].tsx
@@ -1,11 +1,18 @@
 import {festivalTypeMap} from "../../../../utils/game/festival/constant";
 import {FestivalType} from "../../../../utils/game/festival/type";
+import ShareButton from "../../../../components/common/ShareButtons";
+import {useRouter} from "next/router";
 
 interface ResultPageProps {
   festivalType: FestivalType
 }
 
 export default function ResultPage({ festivalType }: ResultPageProps) {
+  const shareUrl: string = "https://music-web-indol.vercel.app/game/festival/result/" + festivalType.id;
+  console.log(festivalType);
+  const router = useRouter();
+  console.log(router.asPath);
+
   return (
     <div>
       <p>{festivalType.name}</p>
@@ -14,6 +21,28 @@ export default function ResultPage({ festivalType }: ResultPageProps) {
           <li key={idx}>{desc}</li>
         )}
       </ul>
+
+      <ShareButton.KaKao
+        content={{
+          title: "Festival Type 게임",
+          description: "내용!",
+          imageUrl: "https://www.esn.org/blog/sites/default/files/imce/576364502a3f379b77617193680e21ff.jpg",
+          link: {
+            mobileWebUrl: shareUrl,
+          }
+        }}
+      >
+        <p>KaKao</p>
+      </ShareButton.KaKao>
+      <ShareButton.Facebook url={shareUrl}>
+        <p>Facebook</p>
+      </ShareButton.Facebook>
+      <ShareButton.Twitter url={shareUrl} text={"Festival Type Game"}>
+        <p>Twitter</p>
+      </ShareButton.Twitter>
+      <ShareButton.Url url={shareUrl}>
+        <p>링크 복사</p>
+      </ShareButton.Url>
     </div>
   )
 }
