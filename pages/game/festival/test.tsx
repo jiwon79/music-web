7d118b06f33acd38c0ac3d069942c55b60d93b0a
jiwon79 <19-079@ksa.hs.kr>
2022-07-02 20:11:01 +0900

    feat: Add progress bar in festival game test page

--- pages/game/festival/test.tsx
+++ pages/game/festival/test.tsx
@@ -3,8 +3,11 @@ import {questionList} from "../../../utils/game/festival/constant";
 import {resultSet} from "../../../utils/game/festival/enums";
 import {koreanOrder} from "../../../utils/game/constant";
 
+import styles from "../../../styles/game/festival/Test.module.scss";
+
 export default function TestPage() {
   const { currentNum, question, nextQuestion } = useGame(questionList, resultSet);
+  const progress = ((currentNum+1)*100/questionList.length).toString();
 
   const answerComponents = question.answers.map((answer, index) =>
     <button
@@ -16,11 +19,14 @@ export default function TestPage() {
   );
 
   return (
-    <div>
+    <div className={styles.container}>
       <p>{koreanOrder[currentNum]}</p>
       <p>{question.question}</p>
       <div>
         {answerComponents}
+      </div>
+      <div className={styles.progress}>
+        <div style={{ width: `${progress}%`}}> </div>
       </div>
     </div>
   )
