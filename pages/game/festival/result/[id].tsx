2f53eb4445e63bb66af620626675ed3b8db7085a
jiwon79 <19-079@ksa.hs.kr>
2022-07-02 21:25:09 +0900

    feat: Add share icon in festival game result page

--- pages/game/festival/result/[id].tsx
+++ pages/game/festival/result/[id].tsx
@@ -1,46 +1,63 @@
 import Link from "next/link";
+import {useRouter} from "next/router";
+
 import ShareButton from "../../../../components/common/ShareButtons";
+import FacebookIcon from "/public/game/festival/facebook_icon.svg"
+import KaKaoIcon from "/public/game/festival/kakao_icon.svg"
+import UrlIcon from "/public/game/festival/url_icon.svg"
+
 import {festivalTypeMap} from "../../../../utils/game/festival/constant";
 import {FestivalType} from "../../../../utils/game/festival/type";
+import {BASE_URL} from "../../../../utils/constants";
+import styles from "../../../../styles/game/festival/Result.module.scss"
 
 interface ResultPageProps {
   festivalType: FestivalType
 }
 
 export default function ResultPage({ festivalType }: ResultPageProps) {
-  const shareUrl: string = "https://music-web-indol.vercel.app/game/festival/result/" + festivalType.id;
+  const router = useRouter();
+  const shareUrl: string = BASE_URL + router.asPath;
 
   return (
     <div>
       <p>{festivalType.name}</p>
-      <ul>
-        {festivalType.descriptions.map((desc, idx) =>
-          <li key={idx}>{desc}</li>
-        )}
-      </ul>
+      <div></div>
+      <div>
+        <ul>
+          {festivalType.descriptions.map((desc, idx) =>
+            <li key={idx}>{desc}</li>
+          )}
+        </ul>
 
-      <ShareButton.KaKao
-        content={{
-          title: "Festival Type 게임",
-          description: "내용!",
-          imageUrl: "https://www.esn.org/blog/sites/default/files/imce/576364502a3f379b77617193680e21ff.jpg",
-          link: {
-            mobileWebUrl: shareUrl,
-          }
-        }}
-      >
-        <p>KaKao</p>
-      </ShareButton.KaKao>
-      <ShareButton.Facebook url={shareUrl}>
-        <p>Facebook</p>
-      </ShareButton.Facebook>
-      <ShareButton.Twitter url={shareUrl} text={"Festival Type Game"}>
-        <p>Twitter</p>
-      </ShareButton.Twitter>
-      <ShareButton.Url url={shareUrl}>
-        <p>링크 복사</p>
-      </ShareButton.Url>
-      <Link href={"../"}>테스트 다시하기</Link>
+        <div>
+          <ShareButton.KaKao
+            content={{
+              title: "Festival Type 게임",
+              description: "내용!",
+              imageUrl: "https://www.esn.org/blog/sites/default/files/imce/576364502a3f379b77617193680e21ff.jpg",
+              link: {
+                mobileWebUrl: shareUrl,
+              }
+            }}
+          >
+            <KaKaoIcon width={24} height={24}/>
+          </ShareButton.KaKao>
+          <ShareButton.Facebook url={shareUrl}>
+            <FacebookIcon width={24} height={24}/>
+          </ShareButton.Facebook>
+          <ShareButton.Twitter url={shareUrl} text={"Festival Type Game"}>
+            <p>Twitter</p>
+          </ShareButton.Twitter>
+          <ShareButton.Url url={shareUrl}>
+            <UrlIcon width={24} height={24}/>
+          </ShareButton.Url>
+        </div>
+
+        <Link href={"../"} passHref>
+          <button>테스트 다시하기</button>
+        </Link>
+      </div>
     </div>
   )
 }
