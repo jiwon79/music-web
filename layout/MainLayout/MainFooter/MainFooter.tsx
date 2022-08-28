5f6625b67bc010dfa519797ff3fdd8d158fadfaa
jiwon79 <19-079@ksa.hs.kr>
2022-08-28 16:23:39 +0900

    :sparkles: Footer 디자인 적용

--- layout/MainLayout/MainFooter/MainFooter.tsx
+++ layout/MainLayout/MainFooter/MainFooter.tsx
@@ -1,28 +1,45 @@
-import styles from './MainFooter.module.scss';
-import Link from "next/link";
 import React from "react";
+import cx from 'classnames';
+import HomeIcon from '/public/icons/ic_home.svg';
+import TimeIcon from '/public/icons/ic_time.svg';
+import PlayListIcon from '/public/icons/ic_playlist.svg';
+import GameIcon from '/public/icons/ic_game.svg';
+import Link from "next/link";
+import styles from './MainFooter.module.scss';
 
-const MainFooter = () => {
+const MainFooter = ({ title }) => {
   return (
     <footer className={styles.footer}>
       <Link href={"#1"}>
-        <div className={styles.footer__item}>
-          홈
+        <div className={cx(
+          styles.footer__item, {[styles.active]: title === 'illusion' }
+        )}>
+          <HomeIcon />
+          <p className={styles.label}>홈</p>
         </div>
       </Link>
       <Link href={"#2"}>
-        <div className={styles.footer__item}>
-          타임X뮤직
+        <div className={cx(
+          styles.footer__item, {[styles.active]: title === 'time' }
+        )}>
+          <TimeIcon />
+          <p className={styles.label}>타임X뮤직</p>
         </div>
       </Link>
       <Link href={"#3"}>
-        <div className={styles.footer__item}>
-          플레이리스트
+        <div className={cx(
+          styles.footer__item, {[styles.active]: title === 'playlist' }
+        )}>
+          <PlayListIcon />
+          <p className={styles.label}>플레이리스트</p>
         </div>
       </Link>
       <Link href={"#4"}>
-        <div className={styles.footer__item}>
-          게임/테스트
+        <div className={cx(
+          styles.footer__item, {[styles.active]: title === 'game' }
+        )}>
+          <GameIcon />
+          <p className={styles.label}>게임/테스트</p>
         </div>
       </Link>
     </footer>
