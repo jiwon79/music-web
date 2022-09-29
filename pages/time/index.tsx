dde98fde49a57e8060d6b2d6723ff6db75b8c493
jiwon79 <19-079@ksa.hs.kr>
2022-09-29 20:14:17 +0900

    :sparkles: Time 페이지에 title wrap 추가

--- pages/time/index.tsx
+++ pages/time/index.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import MainLayout from "layout/MainLayout/MainLayout";
+import styles from "./time.module.scss";
 
 const TimePage = () => {
   return (
     <MainLayout title={'TIME X MUSIC'}>
-      time page
-      <div>
-        <p>매니아 발굴기</p>
-        <Link href={'/time/mania'}><p>전체보기</p></Link>
+      <div className={styles.title__wrap}>
+        <p className={styles.title}>매니아 발굴기</p>
+        <Link href={'/time/mania'}>
+          <p className={styles.link}>전체보기</p>
+        </Link>
       </div>
     </MainLayout>
   )
