93d20bbdb8f3e7ca89cc8590075547ce52be65f7
jiwon79 <19-079@ksa.hs.kr>
2022-09-29 19:53:57 +0900

    :sparkles: Mania 전체 페이지 추가

--- pages/time/index.tsx
+++ pages/time/index.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import MainLayout from "layout/MainLayout/MainLayout";
 
 const TimePage = () => {
   return (
     <MainLayout title={'TIME X MUSIC'}>
       time page
+      <div>
+        <p>매니아 발굴기</p>
+        <Link href={'/time/mania'}><p>전체보기</p></Link>
+      </div>
     </MainLayout>
   )
 }
---
+++ pages/time/mania/index.tsx
@@ -0,0 +1,11 @@
+import MainLayout from "layout/MainLayout/MainLayout";
+
+const ManiaPage = () => {
+  return (
+    <MainLayout title={'매니아 발굴기'}>
+      mania page
+    </MainLayout>
+  );
+}
+
+export default ManiaPage;
