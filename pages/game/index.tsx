ea63f5d3871e9aea04e890ac918db27e23f09cc3
jiwon79 <19-079@ksa.hs.kr>
2022-10-27 20:55:41 +0900

    :lipstick: 게임 페이지 스타일

--- pages/game/index.tsx
+++ pages/game/index.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link"
 import MainLayout from "layout/MainLayout/MainLayout";
+import styles from "./game.module.scss";
 
 export default function GamePage() {
   return (
     <MainLayout title={"게임 / 테스트"}>
-      <p>Game Page</p>
-      <Link href={"/game/listener"}><p>리스너 타입 링크</p></Link>
-      <Link href={"/game/festival"}><p>페스티벌 타입 링크</p></Link>
+      <Link href={"/game/listener"}>
+        <div className={styles.link}>
+          <p>리스너 타입 테스트</p>
+        </div>
+      </Link>
+      <Link href={"/game/festival"}>
+        <div className={styles.link}>
+          <p>페스티벌 타입 테스트</p>
+        </div>
+      </Link>
     </MainLayout>
   )
 }
